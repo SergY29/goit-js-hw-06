044a61f3360e80791887ddd604df694829ed9b4f
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-09-28 16:50:49 +0300

    hw-1

---
+++ src/js/task-01.js
@@ -0,0 +1,21 @@
+const findEl = document.querySelectorAll('#categories .item');
+console.log("Number of categories:", findEl.length);
+
+const allEl = findEl.forEach(element => {
+    console.log("") 
+    console.log("Category:", element.querySelector('h2').textContent);
+    console.log("Elements:", element.querySelectorAll('li').length);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
