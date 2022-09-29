2de820e7cc7fe09d67347bd1f8ea65da407eebe0
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-09-29 14:54:20 +0300

    ex 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingridientsContainerEl = document.querySelector('ul#ingredients');
+
+const newEl = ingredients.map(ingridient => {
+  const listEl = document.createElement('li');
+  listEl.textContent = ingridient;
+  listEl.classList.add('item');
+  return listEl;
+});
+
+ingridientsContainerEl.append(...newEl);
+
+
+
