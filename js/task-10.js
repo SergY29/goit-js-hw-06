f26ba917889f603d5bcf8d532fa7f9e1074d4096
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-03 20:45:24 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,14 @@
+const btnCreateRef = document.querySelector('[data-create]');
+const btnDestroyRef = document.querySelector('[data-destroy]');
+const inputAmountRef = document.querySelector('#controls input');
+const createDivRef = document.querySelector('#boxes');
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
