b408b1f3ec54f23cc14ae1f269709285ae5c2591
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-03 15:16:28 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', changeColor)
+
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+  // color.textContent = body.style.backgroundColor;
+}
+
+
