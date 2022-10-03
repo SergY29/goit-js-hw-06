62108627f39f5ec9d40ac0ac56a308ce23b5a5d5
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-03 18:19:41 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
+const lengthCheck = formEl.getAttribute("data-length");
 
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    if (!event.currentTarget.elements.email.value || !event.currentTarget.elements.password.value) {
+        // console.log('нету ничего')
+        alert('Все поля должны быть заполнены!');
+    }
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const userData = {
+        mail,
+        password,
+    };
+    console.log(userData);
+
+    formEl.reset();
+}
