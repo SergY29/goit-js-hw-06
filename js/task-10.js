ae39eefc5182b5ddf1c856090232464da66b011b
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-04 18:57:56 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -6,18 +6,29 @@ const createDivRef = document.querySelector('#boxes');
 btnCreateRef.addEventListener('click', createBoxes);
 btnDestroyRef.addEventListener('click', destroyBoxes);
 
-function createBoxes(amount) {
-  document.createElement("div");
-  ;
+function createBoxes() {
+  let amount = inputAmountRef.value;
+  for (let i = 0; i < amount; i += 1) {
+    let size = 30 + i * 10;
+    const div = document.createElement("div");
+    const hexColor = getRandomHexColor();
+    createDivRef.append(div);
+    div.style.backgroundColor = hexColor;
+    div.style.width += `${size}px`;
+    div.style.height += `${size}px`;
+    div.classList.add("new-div");
+    div.textContent = 'div';
+  }
+  inputAmountRef.value = '';
 }
 
-function destroyBoxes(amount) {
+function destroyBoxes() {
   createDivRef.innerHTML = '';
 }
 
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
-
 
